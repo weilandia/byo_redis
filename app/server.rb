1b9051287920eaded006370d2b7a282fea68e02d
Nick Weiland <nickweiland@gmail.com>
2023-06-30 12:55:05 -0700

    Bump

--- app/server.rb
+++ app/server.rb
@@ -7,11 +7,13 @@ class YourRedisServer
   MAX_COMMAND_LENGTH = 1024
 
   attr_reader :server, :clients, :store
+  attr_accessor :prev_key
 
   def initialize(port)
     @server = TCPServer.new(port)
     @clients = []
     @store = {}
+    @prev_key = nil
   end
 
   def listen
@@ -41,6 +43,8 @@ class YourRedisServer
     end
 
     instructions = RedisSerializationProtocol.new.parse(resp)
+    print "RESP: #{resp}"
+    puts "INSTRUCTIONS: #{instructions}"
     handle_instructions(instructions, client)
   end
 
@@ -54,8 +58,8 @@ class YourRedisServer
         handle_instructions(instruction[:value], client)
         i += instruction[:value].length
       else
-        puts instructions
         progress = handle_command(instructions, client)
+        puts "CURRENT STORE: #{store}"
         i += progress
       end
     end
@@ -72,11 +76,46 @@ class YourRedisServer
       client.write(bulk_string(instructions[1][:value]))
       2
     when /SET/i
-      store[instructions[1][:value]] = instructions[2][:value]
+      key = instructions[1][:value]
+      value = instructions[2][:value]
+      expiry = nil
+
+      instructions[3..].each_slice(2) do |flag|
+        case flag[0][:value]
+        when /EX/i
+          expiry = flag[1][:value] * 1000
+        when /PX/i
+          expiry = flag[1][:value]
+        end
+      end
+
+      store[key] = { value: value, time: Time.now, expiry: expiry }
       client.write(simple_string("OK"))
-      3
+      instructions.length + 1
+    when /PX/i
+      key = prev_key
+      expiry = instructions[1][:value]
+      obj = store[key]
+      obj[:expiry] = expiry
     when /GET/i
-      client.write(bulk_string(store[instructions[1][:value]]))
+      key = instructions[1][:value]
+      obj = store[key]
+
+      unless obj
+        client.write(null_bulk_string)
+        return 2
+      end
+
+      elapsed = (Time.now - obj[:time]) * 1000.0
+      expired = obj[:expiry] && elapsed > obj[:expiry].to_i
+
+      if expired
+        store.delete(key)
+        client.write(null_bulk_string)
+      else
+        client.write(bulk_string(obj[:value]))
+      end
+
       2
     else
       client.write(simple_string("OK"))
@@ -93,6 +132,14 @@ class YourRedisServer
 
   def error(string)
     "-#{string}\r\n"
+  end
+
+  def null_bulk_string
+    "$-1\r\n"
+  end
+
+  def time_difference_in_milliseconds(start, finish)
+    (finish - start) * 1000.0
   end
 end
 
